f8b2018a5687794bc3b89e5152424d9cb0a6c58b
Thibeau <thibeau.duerinck@icloud.com>
2024-01-22 19:51:19 +0100

    Modify scripts

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -8,42 +8,20 @@ public class CameraSwitch : MonoBehaviour
 {
     public Camera initialCamera;
     public Camera secondCamera;
-    public TextMeshProUGUI countdownText;
-    public float delayTime = 5f; // Vertragingstijd voordat de camera wordt gewisseld
+    public float delayTime = 5f; 
 
     void Start()
     {
-        // Zorg ervoor dat de tweede camera uitgeschakeld is bij het begin
+        
         secondCamera.enabled = false;
-
-        // Start een coroutine om na een vertraging van 'delayTime' van camera te wisselen
-        StartCoroutine(SwitchCameraAfterDelay());
+        
+        Invoke("SwitchCamera", delayTime);
     }
 
-    IEnumerator SwitchCameraAfterDelay()
+    void SwitchCamera()
     {
-        float timer = delayTime;
-
-        while (timer > 0f)
-        {
-            // Update de countdown tekst
-            countdownText.text = Mathf.Ceil(timer).ToString();
-
-            // Zet de positie van de countdown tekst op het scherm volgens de topdown camera
-            Vector3 screenPos = initialCamera.WorldToScreenPoint(transform.position);
-            countdownText.rectTransform.position = screenPos;
-
-            // Verminder de timer en wacht voor een korte periode
-            yield return new WaitForSeconds(1f);
-            timer -= 1f;
-        }
-
-        // Schakel de eerste camera uit en de tweede camera in
+        
         initialCamera.enabled = false;
         secondCamera.enabled = true;
-
-        // Verberg de countdown tekst
-        countdownText.gameObject.SetActive(false);
-
     }
 }
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,29 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Pickup : MonoBehaviour
 {
-   
+    public TextMeshProUGUI counterText;
+    private int coinCounter = 0;  
 
     void Start()
+    {
+        UpdateCounterText();  
+    }
+
+    void Update()
     {
        
     }
 
-    
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             Debug.Log("Coin opgepakt!");
+            coinCounter++;
+            UpdateCounterText();
             Destroy(gameObject);
-
+            Debug.Log(coinCounter);
+            
         }
     }
+
+    
+    void UpdateCounterText()
+    {
+        if (counterText != null)
+        {
+            counterText.text = "Vuilniszakken gevonden: " + coinCounter + "/10";
+        }
+    }
+
+    
 }
